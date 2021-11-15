0edb9ac76fe9b65e7952e75f5609a169e4200774
Bryan Kimani <bryankimani@users.noreply.github.com>
2021-11-15 16:50:42 +0000

    Serve static html file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+console.log(__dirname + '/views/index.html');
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 
 
 
