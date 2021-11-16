56d4f8394dd338498f2d71db34f829ebbdddafb9
Bryan Kimani <bryankimani@users.noreply.github.com>
2021-11-16 04:33:43 +0000

    Serve json object using res.json()

--- myApp.js
+++ myApp.js
@@ -7,7 +7,12 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', function(req, res){
+  res.json({"message": "Hello json"});
+});
+
 app.use('/public', express.static(__dirname + '/public'));
+
 
 
 
