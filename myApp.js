fad5d9b91172c2b758dccfb0520a94fe4ffab5c4
Bryan Kimani <bryankimani@users.noreply.github.com>
2021-11-16 06:33:04 +0000

    Add route logger middleware

--- myApp.js
+++ myApp.js
@@ -4,7 +4,16 @@ require('dotenv').config();
 
 console.log(__dirname + '/views/index.html');
 
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use(function(req, res,next) {
+  console.log(req.method + " " + req.path  + " - " + req.ip);
+  next();
+});
+
 app.get('/', function(req, res) {
+  console.log(req.method);
+  console.log(req.ip);
   res.sendFile(__dirname + '/views/index.html');
 });
 
@@ -15,9 +24,6 @@ app.get('/json', function(req, res){
     res.json({"message": "Hello json"});
   }
 });
-
-app.use('/public', express.static(__dirname + '/public'));
-
 
 
 
